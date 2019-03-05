cc24d2d4524d612db67068255e78fac7d406fd07
saramarin23 <43504914+saramarin23@users.noreply.github.com>
2019-03-05 23:54:52 +0100

    Create lookup.js

---
+++ lookup.js
@@ -0,0 +1,22 @@
+/* Objects can be thought of as a key/value storage, like a dictionary. 
+If you have tabular data, you can use an object to "lookup" values rather than a switch statement or an if/else chain. 
+This is most useful when you know that your input data is limited to a certain range. */
+
+
+function phoneticLookup(val) {
+  var result = {
+
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  };
+
+  result = lookup[val];
+
+  return result;
+}
+
+phoneticLookup("charlie");
