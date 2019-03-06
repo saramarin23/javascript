f5706624c7cda9e92f623133e7be8ed01086a899
saramarin23 <43504914+saramarin23@users.noreply.github.com>
2019-03-06 10:35:35 +0100

    Create manipular_objetos_complejos.js

---
+++ manipular_objetos_complejos.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  
+  // Add record here
+  { 
+    "artist": "John Mayer",
+    "title": "Continuum",
+    "release_year": 2006,
+    "formats": [
+      "CD",
+      "Vynil"
+    ],
+  } 
+]
